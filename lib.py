64f51e8ab4e0651af93fe7d13742af4df759ed19
Lil_Tim <99663083+LilTim0@users.noreply.github.com>
2022-02-15 22:17:57 +0100

    2022.02.15-22:17

--- lib.py
+++ lib.py
@@ -1,12 +1,11 @@
 from random import random
 
-
 def Matricial(matrix1, matrix2):
-  for i in range(len(matrix1, 0)):
-    for j in range(len(matrix2, 1)):
-      z = [len(matrix2, 0), len(matrix1, 1)]
+  for i in range(GetLen(matrix1, 0)):
+    for j in range(GetLen(matrix2, 1)):
+      z = [GetLen(matrix2, 0), GetLen(matrix1, 1)]
       z[i, j] = 0
-      for k in range(len(matrix1, 1)):
+      for k in range(GetLen(matrix1, 1)):
         z[i, j] += matrix1[i, k] * matrix2[k, j]
 
   return z
@@ -29,33 +28,39 @@ def Randn(matrix1, matrix2):
 
 def GetMax(matrix, axis):
   max = 0
-  for i in range(len(matrix, axis)):
-    if(matrix[i] > max):
+  for i in range(GetLen(matrix, axis)):
+    if(matrix[i, 0] > max):
       max = matrix[i]
 
   return max
 
 def MatrixEchelon(matrix, axis):
-  for i in range(len(matrix, axis)):
+  for i in range(GetLen(matrix, axis)):
     matrix[i] /= GetMax(matrix, axis)
 
   return matrix
 
 def MatrixReshape(matrix, numberOfIndexes, keepTheIndexes):
   if(keepTheIndexes):
-    n_list = [numberOfIndexes, len(matrix, 1)]
+    n_list = [numberOfIndexes, GetLen(matrix, 1)]
   else:
-    n_list = [len(matrix, 0) - numberOfIndexes, len(matrix, 1)]
+    n_list = [GetLen(matrix, 0) - numberOfIndexes, GetLen(matrix, 1)]
 
-  for i in range(len(n_list, 0)):
-    for j in range(len(n_list, 1)):
+  for i in range(GetLen(n_list, 0)):
+    for j in range(GetLen(n_list, 1)):
       n_list[i, j] = matrix[i, j]
 
   return matrix
 
 def MatrixRound(matrix, numberOfDecimals):
-  for i in range(len(matrix, 0)):
-    for j in range(len(matrix, 1)):
+  for i in range(GetLen(matrix, 0)):
+    for j in range(GetLen(matrix, 1)):
       matrix[i, j] = round(matrix[i, j], numberOfDecimals)
 
   return matrix
+
+def GetLen(array, axis):
+  if(axis==1):
+    return len(array)
+  else:
+    return len(array[axis-1])
